73935edf1050f9b5c19934aedefcba73fbc77769
johnhilt95 <hilton9505@gmail.com>
2023-10-06 12:35:40 +0530

    Java Learning - Area of Triangle.

---
+++ src/main/java/com/example/java/Java_10.java
@@ -0,0 +1,10 @@
+package com.example.java;
+
+public class Java_10 {
+    public static void main(String[] args){
+        Triangle triangleA=new Triangle(16,8,4,16,12);
+
+        double triangleAArea = triangleA.findArea();
+        System.out.println(triangleAArea);
+    }
+}
